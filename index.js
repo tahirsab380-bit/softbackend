0f35b7c69dd05680d5ae6bcb1794c97e092d21ad
tahirsab380-bit <tahirsab380@gmail.com>
2026-03-08 23:48:05 -0700

    port

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ app.get('/',(req,res)=>{
      res.send("Srver runing")
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
